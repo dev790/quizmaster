f1a773d9407d266e2a32f9311270f182700947f8
Dev Nagda <73869195+dev790@users.noreply.github.com>
2022-01-08 16:10:52 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,143 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The position of a planet when it is nearest to the Sun is called :",
+    answer: "(D) Perihelion",
+    options: [
+      "(A) Apogee",
+
+      "(B) Aphelion",
+      
+      "(C) Perigee",
+      
+      "(D) Perihelion"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The unit of measuring distances between the stars is known as :",
+    answer: "(A) Light year",
+    options: [
+      "(A) Light year",
+
+      "(B) Stellar mile",
+      
+      "(C) Cosmic kilometer",
+      
+      "(D) Galactic unit"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The orbits of planets around the Sun, or of satellites around the Earth can be?",
+    answer: "(B) Circular and elliptic",
+    options: [
+      "(A) Parabolic and hyperbolic",
+      
+      "(B) Circular and elliptic",
+      
+      "(C) Elliptic and parabolic",
+      
+      "(D) Circular and hyperbolic"
+    ]
+  },
+    {
+    numb: 4,
+    question: "On which of the following planets of the solar system does the sun rise in the west and set in the east?",
+    answer: "(D) Venus",
+    options: [
+      "(A) Saturn",
+
+      "(B) Mars",
+
+      "(C) Jupiter",
+
+      "(D) Venus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which one of the following planets takes the same number of days for rotation and revolution?",
+    answer: "(D) Mercury",
+    options: [
+      "(A) Jupiter",
+
+      "(B) Mars",
+
+      "(C) Venus",
+
+      "(D) Mercury"
+    ]
+  },
+     {
+     numb: 6,
+     question: "The eclipse of the Sun occurs",
+     answer: "(B) when the Moon comes between the Sun and the Earth.",
+     options: [
+      "(A) when the Sun comes between the Earth and Moon.",
+
+      "(B) when the Moon comes between the Sun and the Earth.",
+      
+      "(C) when the Earth comes between the Sun and Moon.",
+      
+      "(D)  None of these"
+    ]
+   },
+   {
+    numb: 7,
+    question: "Which of the following does not belong to solar system?",
+    answer: "(B) Nebulae",
+    options: [
+     "(A) Planets",
+
+     "(B) Nebulae",
+     
+     "(C) Comets",
+     
+     "(D) Asteroids"
+   ]
+  },
+  {
+    numb: 8,
+    question: "Our solar system is located in which Galaxy?",
+    answer: "(A) Milky Way",
+    options: [
+     "(A) Milky Way",
+
+     "(B) Andromeda",
+     
+     "(C) Alpha Centauri",
+     
+     "(D) Peroxima Centauri"
+   ]
+  },
+  {
+    numb: 9,
+    question: "The day on which the Sun's direct rays cross the celestial equator is called:",
+    answer: "(C) The Equinox",
+    options: [
+     "(A) The Ecliptic",
+
+     "(B) The Solstice",
+     
+     "(C) The Equinox",
+     
+     "(D) The Aphelion"
+   ]
+  },
+  {
+    numb: 10,
+    question: "Approximately how many miles (or kilometres) are there in a light-year?",
+    answer: "(D) 5.9 trillion (9.5 trillion km)",
+    options: [
+     "(A) 5.9 billion (9.5 billion km)",
+
+     "(B) 590,000 (950,000 km)",
+     
+     "(C) 5.9 million (9.5 million km)",
+     
+     "(D) 5.9 trillion (9.5 trillion km)"
+   ]
+  },
+];
